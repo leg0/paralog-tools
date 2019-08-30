5a0e4d809d81fcf6c7253dd13415ef3b9b556d27
Ahti Legonkov <ahti.legonkov@gmail.com>
2019-08-30 23:00:14 +0300

    paralog-gps-import: Port from .NET Framework to .NET Core

--- paralog-gps-import/src/Program.cs
+++ paralog-gps-import/src/Program.cs
@@ -10,7 +10,7 @@ namespace Paralog_gps
         [Option('d', "database", Required = true, HelpText = "Paralog database file.")]
         public string Database { get; set; }
 
-        [Option('t', "type", Required = false, DefaultValue = "flysight", HelpText = "Type of input file.")]
+        [Option('t', "type", Required = false, Default = "flysight", HelpText = "Type of input file.")]
         public string Type { get; set; }
 
         [Option('i', "input-file", Required = true, HelpText = "Name of input file.")]
@@ -19,7 +19,7 @@ namespace Paralog_gps
         [Option('j', "jump-number", Required = true, HelpText = "Number of jump to modify/create.")]
         public int JumpNumber { get; set; }
 
-        [Option('c', "create", Required = false, DefaultValue = false, HelpText = "Create a record for jump if it doesn't exist.")]
+        [Option('c', "create", Required = false, Default = false, HelpText = "Create a record for jump if it doesn't exist.")]
         public bool ShouldCreateJump { get; set; }
 
         [Option("dropzone", Required = false, HelpText = "Dropzone. Used when new jump is created, otherwise ignored.")]
@@ -27,24 +27,19 @@ namespace Paralog_gps
 
         [Option("aircraft", Required = false, HelpText = "Aircraft name. Used when new jump is created, otherwise ignored.")]
         public string Aircraft { get; set; }
-
-        [HelpOption]
-        public string GetUsage()
-        {
-            return HelpText.AutoBuild(this, (HelpText c) => HelpText.DefaultParsingErrorsHandler(this, c));
-        }
     }
 
     class Program
     {
         static void Main(string[] args)
         {
-            var opt = new Options();
-            if (!CommandLine.Parser.Default.ParseArguments(args, opt))
-            {
-                return;
-            }
+            CommandLine.Parser.Default.ParseArguments<Options>(args)
+                .WithParsed(o => Main(o))
+                .WithNotParsed(errs => {});
+        }
 
+        static void Main(Options opt)
+        {
             var xmlfile = opt.Database;
             var inputFile = opt.InputFile;
             var jumpNumber = opt.JumpNumber;
